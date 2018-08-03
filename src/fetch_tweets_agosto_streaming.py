4a46701a79b199fdff786f122650eae3fc7b5f30
Pablo G Celayes <pablocelayes@gmail.com>
2018-08-02 21:49:25 -0300

    Bajador streaming con auto-update de hashtags

---
+++ src/fetch_tweets_agosto_streaming.py
@@ -0,0 +1,108 @@
+# coding: utf-8
+import json
+import time
+from datetime import date, datetime, timedelta
+
+import tweepy
+from pymongo import MongoClient
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+import json
+from twitter_api_streaming import API_HANDLER as TW
+from collections import Counter
+
+MONGO_HOST= 'mongodb://localhost/twitterdata'
+
+DB = MongoClient(MONGO_HOST).twitterdata
+
+COLLECTION = DB.abortolegal_agosto
+
+HT_THRESHOLD = 0.01  #determina cuales hashtags se usaran para bajar y cuales no a partir de su frecuencia.
+
+HT_UPDATE_INTERVAL = 60 * 60 * 6 
+# HT_UPDATE_INTERVAL = 0
+
+def update_terminos():
+    hashtags_counts = Counter()
+    for t in COLLECTION.find():
+        if "retweeted_status" in t:
+            t = t["retweeted_status"]
+            if "extended_tweet" in t:
+               t = t["extended_tweet"]
+        if "entities" in t:
+            for ht in t["entities"]["hashtags"]:
+                hashtags_counts[ht['text']] += 1
+    
+    n_hashtags = sum(hashtags_counts.values())
+
+    terminos = [t for t, c in hashtags_counts.items() if c * (1.0 / n_hashtags) > HT_THRESHOLD]
+
+    with open('terminos.json','w') as f:
+        json.dump(terminos,f,indent=2)
+
+    return terminos
+
+def load_terminos():
+    with open('terminos_start.json') as f:
+        terminos = json.load(f)
+    
+    try:
+        with open('terminos.json') as f:
+            return list(set(terminos + json.load(f)))
+    except Exception as e:
+        return terminos
+
+class MyStreamListener(tweepy.StreamListener):
+
+    def __init__(self, collection, time_limit=60):
+        self.start_time = time.time()
+        self.limit = time_limit
+        self.coll = collection
+        super(MyStreamListener, self).__init__()
+
+    def on_data(self, data):
+        data = json.loads(data)
+        if not self.coll.find({'id': data['id']}).count():
+            self.coll.insert(data)
+            return True
+        else:
+            return False
+
+    def on_status(self, status):
+        if (time.time() - self.start_time) >= self.limit:
+            print('time is over')
+            return False
+
+    def on_error(self, status):
+        if (time.time() - self.start_time) >= self.limit:
+            print('time is over')
+            return False
+        else:
+            print(status)
+            return True
+
+
+if __name__ == '__main__':
+
+    # for i in range(1,11):
+    LAST_HASHTAG_UPDATE = time.time()
+    i = 0        
+    terminos = load_terminos()
+    while True:
+        i += 1
+        print "Round %04d %s" % (i, datetime.now().strftime("%d/%m: %H:%M"))    
+
+        if time.time() - LAST_HASHTAG_UPDATE > HT_UPDATE_INTERVAL:
+            print "Actualizando lista de hashtags"
+            update_terminos()
+            terminos = load_terminos()    
+            print terminos
+
+        TW.get_fresh_connection()
+        print(TW.conn_.auth.consumer_key)
+
+        listener = MyStreamListener(collection=COLLECTION, time_limit=60) 
+        myStream = tweepy.Stream(auth=TW.conn_.auth, listener=listener)
+        myStream.filter(languages=['es'], track=terminos)
+
+
---
+++ src/twitter_api_streaming.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from tweepy import Cursor, AppAuthHandler, OAuthHandler, API
+from tweepy.error import TweepError
+from random import choice
+from localsettings import AUTH_DATA_STREAMING as AUTH_DATA
+from datetime import datetime
+import time
+
+# Used to switch between tokens to avoid exceeding rate limits
+class APIHandler(object):
+    """docstring for APIHandler"""
+    def __init__(self, auth_data, max_nreqs=10):
+        self.auth_data = auth_data
+        self.index = choice(range(len(auth_data)))
+        self.max_nreqs = max_nreqs
+        self.get_fresh_connection()
+
+    def conn(self):
+        if self.nreqs == self.max_nreqs:
+            self.get_fresh_connection()
+        else:
+            # print("Continuing with API Credentials #%d" % self.index)
+            self.nreqs += 1
+        return self.conn_
+
+    def get_fresh_connection(self):
+        success = False
+        while not success:
+            try:
+                self.index = (self.index + 1) % len(self.auth_data)
+                d = self.auth_data[self.index]
+                print "Switching to API Credentials #%d" % self.index
+
+                auth = OAuthHandler(d['consumer_key'], d['consumer_secret'])
+                auth.set_access_token(d['access_token'], d['access_token_secret'])
+
+                self.conn_ = API(auth_handler=auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+                self.nreqs = 0
+                return self.conn_
+            except TweepError, e:
+                print("Error trying to connect: %s" % e.message)
+                time.sleep(10)
+
+API_HANDLER = APIHandler(AUTH_DATA)
